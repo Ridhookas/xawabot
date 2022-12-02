e7c0fcf4d379438fa7c6b44d2c93d3b890f2cfbb
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-12-03 02:30:47 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -152,12 +152,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.jpg')
-    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://github.co/Rexashh/xawabot', 'Github', null, null, [
-      ['Donate', '/donasi'],
-      ['Speed', '/ping'],
-      ['Owner', '/owner']
-    ], m)
+    conn.reply(m.chat, text.trim(), m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
