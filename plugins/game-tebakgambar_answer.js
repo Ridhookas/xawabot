15c6b5719d78396ca97f9dae35c88dbb5c6c27a5
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-12-03 13:35:54 +0700

    Update game-tebakgambar_answer.js

--- plugins/game-tebakgambar_answer.js
+++ plugins/game-tebakgambar_answer.js
@@ -3,35 +3,25 @@ import db from '../lib/database.js'
 
 const threshold = 0.72
 export async function before(m) {
-    let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*hint/i.test(m.quoted.text) || /.*hint/i.test(m.text))
-        return !0
-    this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-    if (!(id in this.tebakgambar))
-        return this.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttonTebakgambar, m)
-    if (m.quoted.id == this.tebakgambar[id][0].id) {
-        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
-        if (isSurrender) {
-            clearTimeout(this.tebakgambar[id][3])
-            delete this.tebakgambar[id]
-            return this.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonTebakgambar, m)
-        }
-        let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            db.data.users[m.sender].exp += this.tebakgambar[id][2]
-            this.sendButton(m.chat, `*Benar!*\n+${this.tebakgambar[id][2]} XP`, author, null, buttonTebakgambar, m)
-            clearTimeout(this.tebakgambar[id][3])
-            delete this.tebakgambar[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
-            m.reply(`*Dikit Lagi!*`)
-        else
-            this.sendButton(m.chat, `*Salah!*`, author, null, [
-                ['hint', '/hint'],
-                ['nyerah', 'menyerah']
-            ], m)
-    }
-    return !0
+      let id = m.chat
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.contentText)) return !0
+  this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
+  if (!(id in this.tebakgambar)) return m.reply('Soal itu telah berakhir')
+  if (/^nyerah$/i.test(m.text)) {
+    await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.tebakgambar[id][1].jawaban))}`.trim(), author, null, buttonTebakgambar, m').then(() => { delete this.tebakgambar[id] })
+  }
+  // if (m.quoted.id == this.tebakgambar[id][0].id) { // undefined T_T
+  let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
+  if (['.hint', 'BANTUAN', ''].includes(m.text)) return !0
+  if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
+    global.DATABASE.data.users[m.sender].exp += this.tebakgambar[id][2]
+    await this.sendButton(m.chat, `*Benar!* +${this.tebakgambar[id][2]} XP`, author, null, buttonTebakgambar, m)
+    clearTimeout(this.tebakgambar[id][3])
+    delete this.tebakgambar[id]
+  } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+  else m.reply(`*Salah!*`)
+  // }
+  return !0
 }
 export const exp = 0
 
