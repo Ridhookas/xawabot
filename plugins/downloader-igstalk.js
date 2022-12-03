a73e6197f54250090a8acb46dd95705c214ba78d
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-12-03 12:57:54 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,28 +1,15 @@
-import { instagramStalk } from '@bochilteam/scraper'
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} Beni_230`
 
-let handler= async (m, { args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
-    const {
-        username,
-        name,
-        description,
-        followersH,
-        followingH,
-        postsH,
-    } = await instagramStalk(args[0])
-    m.reply(`
-${name} *(${username})*
-https://instagram.com/${username.replace(/^@/, '')}
-*${followersH}* Followers
-*${followingH}* Following
-*${postsH}* Posts
-*Bio:* ${description}
-`.trim())
+  let res = await fetch(global.API('zekais', '/igs', { username: args[0] }))
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  let json = await res.json()
+  if (json.status != 200) throw json
+  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/beni_230`, m, 0, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
 }
-
-handler.help = ['igstalk'].map(v => v + ' <username>')
+handler.help = ['igstalk <username>']
 handler.tags = ['downloader']
-
 handler.command = /^(igstalk)$/i
-
-export default handler
+handler.limit = true
+module.exports = handler
